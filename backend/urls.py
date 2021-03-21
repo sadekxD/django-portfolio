e3ae3f8ad647283e784c771b65d081d25ea99428
unknown <sadekirfan3@gmail.com>
2021-03-21 10:27:58 +0600

    'commit'

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from core.views import ProjectView, ContactView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/project-list/", ProjectView.as_view(), name="project-list"),
     path("api/contact/", ContactView.as_view(), name="contact"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
